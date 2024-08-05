e35cd3faa406523ade42a55292af37ca8c570bc5
Kyle-Nord <Kyle-Nord@users.noreply.github.com>
2024-08-05 18:46:28 -0500

    fix path of test structure

--- demos/structure_relaxation.py
+++ demos/structure_relaxation.py
@@ -7,7 +7,7 @@ DATA_DIR = os.path.join("output", "mlp-relaxation")
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-TEST_STRUC = os.path.join("mp-18767-LiMnO2.cif")
+TEST_STRUC = os.path.join("cifs", "mp-18767-LiMnO2.cif")
 SAVE_TRAJ = os.path.join("output", "mlp-relaxation", "chgnet_traj.traj")
 
 
