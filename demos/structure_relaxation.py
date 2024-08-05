774532f9bdfa0b5f402f67936b403aeceb35b8f1
Kyle Noordhoek <Kyle-Nord@users.noreply.github.com>
2024-08-05 18:36:08 -0500

    migrate demo from pydmclab

---
+++ demos/structure_relaxation.py
@@ -0,0 +1,45 @@
+import os
+from pydmclab.core.struc import StrucTools
+from pydmc_ml.dynamics import CHGNetRelaxer
+
+
+DATA_DIR = os.path.join("output", "mlp-relaxation")
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+TEST_STRUC = os.path.join("mp-18767-LiMnO2.cif")
+SAVE_TRAJ = os.path.join("output", "mlp-relaxation", "chgnet_traj.traj")
+
+
+def main():
+    # Load structure
+    initial_structure = StrucTools(TEST_STRUC).structure
+
+    # Perturb structure
+    perturbed_structure = initial_structure.perturb(0.3)
+
+    # Initialize relaxer
+    relaxer = CHGNetRelaxer(model="0.3.0", use_device="mps")
+
+    # Relax structure
+    results = relaxer.relax(
+        perturbed_structure,
+        fmax=0.1,
+        steps=50,
+        traj_path=SAVE_TRAJ,
+        verbose=True,
+    )
+
+    # Get results from the observer
+    observer = results["trajectory"]
+    print("CHGNet took {} steps to converge.".format(len(observer)))
+
+    # Get initial and final energies
+    print("Initial energy: {:.3f} eV".format(observer.energies[0]))
+    print("Final energy: {:.3f} eV".format(observer.energies[-1]))
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
